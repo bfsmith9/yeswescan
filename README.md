4d96f46e9ebae2965e9fbf0425654bf168a9e449
bfsmith9 <21973169+bfsmith9@users.noreply.github.com>
2023-02-02 19:24:50 -0500

    More cleanup

--- README.md
+++ README.md
@@ -1,35 +1,46 @@
-# yeswescan
+# yeswecan! 
+Barry Smith February 2023 
 
-## NOTES
+## Installing Python on Windows (choose Python 3 - more modern)
 
-- The quantity will only be 1-3 characters. No thousands or more
-- After the output of one without any number, put a "1" for the quantity. 
+<https://www.python.org/downloads/windows/>
 
-## Sample input file
+## Sample input file  
 9780671656607  
 9780449222713  
 42  
 9780449222719  
 
-## Sample output file
-### Header
-Unused field,Sequence,Barcode,Quantity,Dept/Cat,User,Price,Area,Field
+## Header
 
-### Output complete
-Unused field,Sequence,Barcode,Quantity,Dept/Cat,User,Price,Area,Field  
+Unused Field, Sequence, Barcode, Quantity, Dept/Cat, User, Price, Area,  
+
+## Sample output file
+
+Unused Field, Sequence, Barcode, Quantity, Dept/Cat, User, Price, Area,  
 ,,9780671656607,,123456,222,6.60,  
 ,,9780671656607,1,123456,222,6.60,  
 ,,9780449222713,37,123456,222,5.99,  
-,,9780459222714,314,123456,222,5.99,
+,,9780459222714,314,123456,222,5.99,  
 
 - Quantity is always 1-3 characters, no more
-- Barcode is always longer
-- If there's no quantity, put a "1"
+- Barcode is always 4 characters or longer
+- If there's no quantity, put a "1" as the default quantity
 
 ## Algorithm 
+- Start GUI input boxes to get user, dept 
 - import input file
-- output a header to the output file 
-- Loop, reading each line
+    - Read it once to get the last line; close
+    - Open again to read each line 
+- Open output file
+- Output a header to the output file 
+- Read a line
 - If line > 3, save to isbn variable
-- If next line <= 3, it's a quantity. Save it to `found_qty` variable, and output the isbn and the `found_qty` to the output file in the expected .csv format
+- If next line <= 3, it's a quantity. Save it to found_qty variable, and output the isbn and the found_qty to the output file in the expected .csv format
 - Else, output the isbn variable and the default_qty variable (1) to the output file in the expected .csv file
+
+## NOTES
+- Python's (len) can include the newline character
+
+## 2/1/2023
+- Customer wants Dept/Cat & User as well, which are the fields directly after QTY, respectively. After that there are two more fields we won't fill in, "Price" & "Area", so just two more commas added to the end.
--- yeswescan.py
+++ yeswescan.py
@@ -1,48 +1,52 @@
 #! /usr/bin/python3
 
 """
-Installing Python on Windows (choose Python 3 - more modern)
+yeswecan! 
+Barry Smith February 2023 
 
-https://learn.microsoft.com/en-us/windows/python/beginners
+## Installing Python on Windows (choose Python 3 - more modern)
 
-https://www.python.org/downloads/windows/
+<https://www.python.org/downloads/windows/>
 
-## Sample input file
-9780671656607
-9780449222713
-42
-9780449222719
+## Sample input file  
+9780671656607  
+9780449222713  
+42  
+9780449222719  
+
+## Header
+
+Unused Field, Sequence, Barcode, Quantity, Dept/Cat, User, Price, Area,  
 
 ## Sample output file
-### Header
-Unused Field, Sequence, Barcode, Quantity, Dept/Cat, User, Price, Area,
- 
-a,b,isbn,qty,c,d,e,f
-,,9780671656607,,123456,222,6.60,
-,,9780671656607,1,123456,222,6.60,
-,,9780449222713,37,123456,222,5.99,
-,,9780459222714,314,123456,222,5.99,
+
+Unused Field, Sequence, Barcode, Quantity, Dept/Cat, User, Price, Area,  
+,,9780671656607,,123456,222,6.60,  
+,,9780671656607,1,123456,222,6.60,  
+,,9780449222713,37,123456,222,5.99,  
+,,9780459222714,314,123456,222,5.99,  
 
 - Quantity is always 1-3 characters, no more
-- Barcode is always longer
-- If there's no quantity, put a "1"
+- Barcode is always 4 characters or longer
+- If there's no quantity, put a "1" as the default quantity
 
 ## Algorithm 
+- Start GUI input boxes to get user, dept 
 - import input file
-- output a header to the output file 
+    - Read it once to get the last line; close
+    - Open again to read each line 
+- Open output file
+- Output a header to the output file 
 - Read a line
 - If line > 3, save to isbn variable
-- If next line <= 3, it's a quantity. Save it to found_qty variable., and output the isbn and the found_qty to the output file in the expected .csv format
+- If next line <= 3, it's a quantity. Save it to found_qty variable, and output the isbn and the found_qty to the output file in the expected .csv format
 - Else, output the isbn variable and the default_qty variable (1) to the output file in the expected .csv file
 
 ## NOTES
 - Python's (len) can include the newline character
-- Working so far, but have to add a 1 on the end if the last 
-string isn't a qty (if it's an ISBN). 
-    - OK - fixed
 
 2/1/2023
-- They want Dept/Cat & User as well, which are the fields directly after QTY, respectively. After that there are two more fields we won't fill in, "Price" & "Area", so just two more commas added to the end.
+- Customer wants Dept/Cat & User as well, which are the fields directly after QTY, respectively. After that there are two more fields we won't fill in, "Price" & "Area", so just two more commas added to the end.
 
 """
 
